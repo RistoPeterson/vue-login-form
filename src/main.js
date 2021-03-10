fddcf13fa607b4f50ba68ee395cbf08b1e153912
Risto Peterson <you@example.com>
2021-03-10 14:14:15 +0200

    Login Form

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+/* eslint-disable no-unused-vars */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBy7kk1h3oZ4Q7yAjWIZlp9HssDe34AA-4",
+    authDomain: "vue-login-form-952ba.firebaseapp.com",
+    projectId: "vue-login-form-952ba",
+    storageBucket: "vue-login-form-952ba.appspot.com",
+    messagingSenderId: "306164291285",
+    appId: "1:306164291285:web:7a89efea0fd3db354d745c"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="login">
+     <form>
+         <input type="text" placeholder="Email" />
+         <input type="text" placeholder="Password" />
+         <button>Login</button>
+     </form>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
